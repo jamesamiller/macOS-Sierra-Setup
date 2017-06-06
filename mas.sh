c21b4addfb629434196f6cdc5b66cbfb60d20c92
James A Miller <millerja@uah.edu>
2017-06-06 11:30:04 -0500

    Eliminated the `sudo` initial login and timer

--- mas.sh
+++ mas.sh
@@ -4,12 +4,6 @@
 
 # Make script executable: chmod u+x mas.sh.
 
-# Ask for the administrator password upfront.
-sudo -v
-
-# Keep alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 # Double check for Homebrew, and install if we don't have it.
 if test ! $(which brew); then
   echo "Installing homebrew..."
--- prereq.sh
+++ prereq.sh
@@ -5,17 +5,11 @@
 
 # Make script executable: chmod u+x prereq.sh
 
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until `osxprep.sh` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 # Update the OS.
 echo "------------------------------"
 echo "Updating OSX...  If this requires a restart, run the script again."
 # Install all available updates
-sudo softwareupdate -ia --verbose
+sudo softwareupdate -i -a --verbose
 # Install only recommended available updates
 #sudo softwareupdate -ir --verbose
 
